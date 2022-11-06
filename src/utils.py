6f9d3abb2e36a2d3ae61e8bab46d1e2f95edf956
imag-fff <yuki.xyz@me.com>
2022-11-07 04:36:44 +0900

    feat: implement reduce_noise()

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,21 @@
 from copy import deepcopy
+from enum import Enum
+from pprint import pprint
 from typing import Optional
 
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from numpy import ndarray
 from obspy.clients.fdsn.client import Client
+from obspy.core import UTCDateTime
 from obspy.core.stream import Stream
 from obspy.core.utcdatetime import UTCDateTime
+from scipy import signal
 
 from src.pdart_utils import linear_interpolation
+
+plt.rcParams["font.size"] = 24
 
 
 def remove_response(
@@ -55,3 +64,112 @@ def remove_response(
     except Exception as e:
         print(f"========== ERROR ==========\n\n{e}")
         return
+
+
+class NoiseReductionMethod(Enum):
+    ENVELOPE = "envelope"
+    EWM = "ewm"
+
+
+def reduce_noise(
+    stream: Stream,
+    method: NoiseReductionMethod = NoiseReductionMethod.ENVELOPE.value,
+    times: int = 5,
+    inplace: bool = False,
+    is_plot: bool = False,
+) -> Stream:
+    st = stream.copy() if not inplace else stream
+    d = st[0].data
+    if is_plot:
+        ticks, datetime_ticks = _get_datetime_ticks(st)
+
+        def _plot(
+            data_prev: Optional[ndarray] = None,
+            data_current: Optional[ndarray] = None,
+            threshold: Optional[ndarray] = None,
+            title: Optional[str] = None,
+        ) -> None:
+            fig = plt.figure(figsize=(24, 8))
+            ax = fig.add_subplot(1, 1, 1)
+
+            if data_prev is not None:
+                ax.plot(data_prev, color="red", label="before")
+            if data_current is not None:
+                ax.plot(data_current, color="black", label="after")
+            if threshold is not None:
+                ax.plot(threshold, color="#32CD32", label="threshold")
+                ax.plot(-threshold, color="#32CD32")
+            if title:
+                ax.set_title(title)
+
+            ax.set_xticks(ticks, datetime_ticks)
+            ax.set_xlabel("Datetime (UTC)")
+            ax.set_ylim(-3e-7, 3e-7)
+            ax.set_ylabel("$m/s$")
+            ax.grid(which="major")
+            ax.legend(loc="upper right")
+            plt.show()
+
+        _plot(data_current=d, title="Before")
+
+    if method == NoiseReductionMethod.ENVELOPE.value:
+        # hyperparameters
+        N_SECONDS = 60
+        N_SAMPLES = int(st[0].stats.sampling_rate * N_SECONDS)  # window size
+        ENV_MUL, STD_MUL = 3, 1.5
+
+        for t in range(times):
+            if is_plot:
+                d_before = deepcopy(d)
+
+            threshold = (
+                np.convolve(
+                    np.ones(N_SAMPLES) / N_SAMPLES, abs(signal.hilbert(d)), mode="same"
+                )
+                * ENV_MUL
+                + np.var(d) * STD_MUL
+            )
+            d[np.abs(d) > threshold] = 0
+
+            if is_plot:
+                _plot(
+                    d_before,
+                    d,
+                    threshold,
+                    title=f"Noise reduction with {method} ({t+1} times)",
+                )
+
+    elif method == NoiseReductionMethod.EWM.value:
+        # hyperparameters
+        N_SECONDS = 20
+        N_SAMPLES = int(st[0].stats.sampling_rate * N_SECONDS)  # window size
+        STD_MUL = 4
+
+        d = np.append(d, np.zeros(N_SAMPLES))  # add dummy data
+        d = pd.Series(d)
+        for t in range(times):
+            if is_plot:
+                d_before = deepcopy(d)
+
+            threshold = (d.ewm(span=N_SAMPLES).std() * STD_MUL)[N_SAMPLES:].reset_index(
+                drop=True
+            )
+            d[:-N_SAMPLES][d.abs()[:-N_SAMPLES] > threshold] = 0
+
+            if is_plot:
+                _plot(
+                    d_before,
+                    d,
+                    threshold,
+                    title=f"Noise reduction with {method} ({t+1} times)",
+                )
+
+        d = np.array(d)
+        d = d[:-N_SAMPLES]  # remove dummy data
+
+    st[0].data = d
+    if is_plot:
+        _plot(data_current=d, title="After")
+
+    if not inplace:
+        return st
